6a80598f6b6b93536706b74f962e410ed0b8852e
Thomas Jordan <info@jordanonline.de>
2025-07-16 03:50:25 +0200

    init Projekt

---
+++ lib/quiz_brain.dart
@@ -0,0 +1,31 @@
+import 'question.dart';
+
+int _questionNumber = 0;
+
+class QuizBrain {
+  final List<Question> _questionBank = [
+    Question(
+      'Kann man eine Kuh die Treppe hinunter, aber nicht hinauf führen?',
+      false,
+    ),
+    Question(
+      'Etwa ein Viertel der menschlichen Knochen befinden sich in den Füßen?',
+      true,
+    ),
+    Question('Das Blut einer Schnecke ist grün?', true),
+  ];
+
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length - 1) {
+      _questionNumber++;
+    }
+  }
+
+  String getQuestionText() {
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool getQuestionAnswer() {
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+}
